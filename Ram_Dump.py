db3234bc0966f1edbb52487f2f6d0370e99bd449
Aditya Kate <aditya.kate4703@gmail.com>
2023-04-09 16:30:21 +0530

    added .gitignore file with Output folder, done some optimization of Ram_Dump.py (in creation of Output folder and Subprocess run module)

--- Ram_Dump.py
+++ Ram_Dump.py
@@ -1,38 +1,31 @@
 #!/usr/bin/env python
 from sys import platform, exit
-#from subprocess import Popen
-from subprocess import run #Using run method for better RAM Management
+from subprocess import run
 from os import path, makedirs, getcwd
+from datetime import datetime
 
 # get the current working directory
 cwd = getcwd()
 
 def dump_ram():
     # create the "Output" folder if it doesn't exist
-    if not path.exists("Output"):
-        makedirs("Output")
-#----------------------------------------------------------------------------------
+    makedirs("Output", exist_ok=True)
+    
     # create a file name with current date and time
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = f"mem_{current_time}.raw"
-#----------------------------------------------------------------------------------    
-   
+    file_name = f"memdump_{current_time}.raw" 
+
     # create a file path relative to the current working directory
-    #file_path = path.join(cwd, "Output/mem.raw")
     file_path = path.join(cwd, "Output", file_name) #File path with date and time stamp
         
     # Check the current operating system
     if platform.startswith('win'):
         # Execute the Windows RAM dump code
-        """process = Popen(['./tools/winpmem_mini_x64_rc2.exe', file_path])
-        process.wait()"""
-        run(['./tools/winpmem_mini_x64_rc2.exe', file_path], check=True)
+        process = run(['./tools/winpmem_mini_x64_rc2.exe', file_path])
         
     elif platform.startswith('linux'):
         # Execute the Linux RAM dump code
-        """process = Popen(['./tools/avml-minimal', file_path])
-        process.wait()"""
-        run(['./tools/avml-minimal', file_path], check=True)
+        process = run(['./tools/avml-minimal', file_path], check=True)
         
     elif platform.startswith('darwin'):
         # Execute the Mac RAM dump code
